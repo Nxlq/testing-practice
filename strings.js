72142a4e03f3543f5fe0311fc6851bc9f2f2e3c1
JeffW <116536323+Nxlq@users.noreply.github.com>
2023-04-24 16:20:40 -0400

    create test case for when a string that is already capitalized is passed in to the capitalize function and edit capitalize function to handle it accordingly

--- strings.js
+++ strings.js
@@ -1,5 +1,6 @@
 export function capitalize(string) {
   if (!string) return;
+  if (string.charAt(0) === string.charAt(0).toUpperCase())
+    return "That string is already capitalized :D";
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
--- strings.test.js
+++ strings.test.js
@@ -3,3 +3,7 @@ import { capitalize } from "./strings";
 test("Capitalizes the first character in a string", () => {
   expect(capitalize("testing")).toBe("Testing");
 });
+
+test("If the string is already capitalized", () => {
+  expect(capitalize("Already")).toBe("That string is already capitalized :D");
+});
