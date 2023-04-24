a1d233e2dd63ee55cfa866a94c546436bbee7ad3
JeffW <116536323+Nxlq@users.noreply.github.com>
2023-04-24 19:40:16 -0400

    create caesarCipher function that ciphers a string and passes all jest tests

--- strings.js
+++ strings.js
@@ -8,3 +8,52 @@ export function capitalize(string) {
 export function reverseString(string) {
   return string.split("").reverse().join("");
 }
+
+export function caesarCipher(string, shiftAmount) {
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  const cipher = string
+    .split("")
+    .map((letter) => {
+      // if the letter is a special character
+      if (!alphabet.includes(letter)) return letter;
+      let newIndex = alphabet.indexOf(letter.toLowerCase()) + shiftAmount;
+
+      // check if the new Index is shifted beyond the the last letter in the alphabet
+      if (newIndex > 26) newIndex -= 26;
+      const newLetter = alphabet[newIndex];
+
+      if (letter === letter.toUpperCase()) return newLetter.toUpperCase();
+      return newLetter;
+    })
+    .join("");
+
+  return cipher;
+}
